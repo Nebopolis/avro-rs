24022fa339ab44c35433d06e6744c8511b3cb376
Ben Evans <bevans@zendesk.com>
2015-12-08 00:39:04 -0600

    more stubbing out methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::str::FromStr;
+use std::error::Error;
+use std::fmt;
 
 enum Order {
   Ascending,
@@ -65,13 +68,47 @@ trait Valid {
   fn valid(&self) -> bool;
 }
 
-trait ToType {
-  fn to_type(&self) -> Type;
+// trait ToType {
+//   fn to_type(&self) -> Option<Type>;
+// }
+
+// impl <'a>ToType for &'a str {
+//   fn to_type(&self) -> Option<Type> {
+//     match &self.as_ref() {
+//       "null" => Type::Null,
+//       _     => Type::Null
+
+//     }
+//   }
+// }
+
+struct ParseTypeError<'a> {
+  cause: Option<&'a Error>
 }
 
-impl <'a>ToType for &'a str {
-  fn to_type(&self) -> Type {
-    return Type::Null;
+impl <'a>fmt::Debug for ParseTypeError<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "hi")
+    }
+}
+
+impl <'a> fmt::Display for ParseTypeError<'a> {
+      fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "hi")
+    }
+}
+
+impl <'a>Error for ParseTypeError<'a> {
+
+  fn description(&self) -> &str {
+    "hi"
+  }
+}
+
+impl <'a>FromStr for Type<'a> {
+  type Err = ParseTypeError<'a>;
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    Ok(Type::Null)
   }
 }
 
