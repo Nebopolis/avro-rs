8543733596f3b74a9369e2ac4f4f748b943dc8c8
Ben Evans <bevans@zendesk.com>
2016-01-20 23:49:20 -0600

    reworking things mostly from scratch

--- src/lib.rs
+++ src/lib.rs
@@ -1,157 +1,118 @@
-#![feature(custom_derive, plugin)]
-#![plugin(serde_macros)]
-#![feature(custom_attribute)]
-use std::str::FromStr;
-use std::error::Error;
-use std::default;
-use std::fmt;
-use std::vec;
-use std::collections::HashMap;
-extern crate serde;
+// NULL    = 'null'
+// BOOLEAN = 'boolean'
+// STRING  = 'string'
+// BYTES   = 'bytes'
+// INT     = 'int'
+// LONG    = 'long'
+// FLOAT   = 'float'
+// DOUBLE  = 'double'
+// FIXED   = 'fixed'
+// ENUM    = 'enum'
+// RECORD  = 'record'
+// ERROR   = 'error'
+// ARRAY   = 'array'
+// MAP     = 'map'
+// UNION   = 'union'
 
-
-extern crate serde_json;
-use serde_json::Value;
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Protocol {
-  name: String,
-  protocol: Option<String>,
-  doc: Option<String>,
-  types: Vec<Type>,
-  messages: Option<HashMap<String, Message>>
+mod schema {
+  enum Primitive {
+    Null,
+    Boolean,
+    String,
+    Bytes,
+    Int,
+    Long,
+    Float,
+    Double
+  }
+  enum Named {
+    Fixed,
+    Enum,
+    Record,
+    Error
+  }
+  enum Complex {
+    Array,
+    Map,
+    Union,
+    Request,
+    ErrorUnion
+  }
+  enum Type {
+    Primitive(Primitive),
+    Named(Named),
+    Complex(Complex)
+  }
+  enum Sort {
+    Ascending,
+    Descending,
+    Ignore
+  }
+  
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Message {
-  doc: Option<String>,
-  request: Vec<Field>,
-  response: Type,
-  errors: Vec<Type>,
-  one_way: Option<bool>
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename="order")]
-enum Order {
-  #[serde(rename="ascending")]
-  Ascending,
-  #[serde(rename="descending")]
-  Descending,
-  #[serde(rename="ignore")]
-  Ignore
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename="type")]
-enum Type {
-  #[serde(rename="null")]
-  Null,
-  #[serde(rename="boolean")]
-  Boolean,
-  #[serde(rename="int")]
-  Int,
-  #[serde(rename="long")]
-  Long,
-  #[serde(rename="float")]
-  Float,
-  #[serde(rename="double")]
-  Double,
-  #[serde(rename="bytes")]
-  Bytes,
-  #[serde(rename="string")]
-  String,
-  #[serde(rename="record")]
-  Record(Record),
-  #[serde(rename="enum")]
-  Enum(Enum),
-  #[serde(rename="array")]
-  Array(Array),
-  #[serde(rename="map")]
-  Map(Map),
-  #[serde(rename="union")]
-  Union(Vec<Type>),
-  #[serde(rename="fixed")]
-  Fixed(Fixed)
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename="field")]
-struct Field {
-  name: String,
-  doc: Option<String>,
-  #[serde(rename="type")]
-  av_type: Type,
-  default: String,
-  order: Option<Order>
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename="record")]
-struct Record {
-    name: String,
-    namespace: Option<String>,
-    doc: Option<String>,
-    aliases: Option<Vec<String>>,
-    fields: Vec<Field>
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename="enum")]
-struct Enum {
-    name: String,
-    namespace: Option<String>,
-    doc: Option<String>,
-    aliases: Option<Vec<String>>,
-    symbols: Vec<String>
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename="array")]
-struct Array {
-  items: Box<Type>
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename="map")]
-struct Map {
-  values: Vec<String>
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename="fixed")]
-struct Fixed {
-  name: String,
-  namespace: Option<String>,
-  aliases: Option<Vec<String>>,
-  size: i32
-}
 
 #[test]
 #[should_panic]
 fn it_works() {
-  let arr: Array = Array {
-    items: Box::new(Type::Null)
-  };
-  let field: Field = Field {
-    name: String::from("null_array"),
-    doc: None,
-    av_type: Type::Array(arr),
-    default: String::from("null"),
-    order: Some(Order::Ascending)
-  };
-  let record: Record = Record {
-    name: String::from("record"),
-    namespace: None,
-    doc: None,
-    aliases: None,
-    fields: vec![field]
-  };
-
-  let serialized = serde_json::to_string(&record).unwrap();
-  println!("{}", serialized);
-  let test = String::from("{\"namespace\":\"com.acme\",\"protocol\":\"HelloWorld\",\"doc\":\"ProtocolGreetings\",\"types\":[{\"name\":\"Greeting\",\"type\":\"record\",\"fields\":[{\"name\":\"message\",\"type\":\"string\"}]},{\"name\":\"Curse\",\"type\":\"error\",\"fields\":[{\"name\":\"message\",\"type\":\"string\"}]}],\"messages\":{\"hello\":{\"doc\":\"Sayhello.\",\"request\":[{\"name\":\"greeting\",\"type\":\"Greeting\"}],\"response\":\"Greeting\",\"errors\":[\"Curse\"]}}");
-  let deserialized: Record = serde_json::from_str(&serialized).unwrap();
-  println!("{:?}", deserialized);
-  println!("{}",test);
 }
+
+// # Request and error unions are part of Avro protocols:
+// REQUEST = 'request'
+// ERROR_UNION = 'error_union'
+
+// PRIMITIVE_TYPES = frozenset([
+//   NULL,
+//   BOOLEAN,
+//   STRING,
+//   BYTES,
+//   INT,
+//   LONG,
+//   FLOAT,
+//   DOUBLE,
+// ])
+
+// NAMED_TYPES = frozenset([
+//   FIXED,
+//   ENUM,
+//   RECORD,
+//   ERROR,
+// ])
+
+// VALID_TYPES = frozenset.union(
+//   PRIMITIVE_TYPES,
+//   NAMED_TYPES,
+//   [
+//     ARRAY,
+//     MAP,
+//     UNION,
+//     REQUEST,
+//     ERROR_UNION,
+//   ],
+// )
+
+// SCHEMA_RESERVED_PROPS = frozenset([
+//   'type',
+//   'name',
+//   'namespace',
+//   'fields',     # Record
+//   'items',      # Array
+//   'size',       # Fixed
+//   'symbols',    # Enum
+//   'values',     # Map
+//   'doc',
+// ])
+
+// FIELD_RESERVED_PROPS = frozenset([
+//   'default',
+//   'name',
+//   'doc',
+//   'order',
+//   'type',
+// ])
+
+// VALID_FIELD_SORT_ORDERS = frozenset([
+//   'ascending',
+//   'descending',
+//   'ignore',
+// ])
