1ac4ff5b5afc08708b278d418330037c15eecf2d
Ben Evans <bevans@zendesk.com>
2016-01-22 23:12:26 -0500

    more work on schema

--- src/lib.rs
+++ src/lib.rs
@@ -48,71 +48,25 @@ mod schema {
     Descending,
     Ignore
   }
-  
-}
 
+  static SCHEMA_RESERVED_PROPS: [&'static str; 9] = ["type",
+  "name",
+  "namespace",
+  "fields",
+  "items",
+  "size",
+  "symbols",
+  "values",
+  "doc"];
+  static FIELD_RESERVED_PROPS: [&'static str; 5] = [
+  "default",
+   "name",
+   "doc",
+   "order",
+  "type",];
+}
 
 #[test]
 #[should_panic]
 fn it_works() {
 }
-
-// # Request and error unions are part of Avro protocols:
-// REQUEST = 'request'
-// ERROR_UNION = 'error_union'
-
-// PRIMITIVE_TYPES = frozenset([
-//   NULL,
-//   BOOLEAN,
-//   STRING,
-//   BYTES,
-//   INT,
-//   LONG,
-//   FLOAT,
-//   DOUBLE,
-// ])
-
-// NAMED_TYPES = frozenset([
-//   FIXED,
-//   ENUM,
-//   RECORD,
-//   ERROR,
-// ])
-
-// VALID_TYPES = frozenset.union(
-//   PRIMITIVE_TYPES,
-//   NAMED_TYPES,
-//   [
-//     ARRAY,
-//     MAP,
-//     UNION,
-//     REQUEST,
-//     ERROR_UNION,
-//   ],
-// )
-
-// SCHEMA_RESERVED_PROPS = frozenset([
-//   'type',
-//   'name',
-//   'namespace',
-//   'fields',     # Record
-//   'items',      # Array
-//   'size',       # Fixed
-//   'symbols',    # Enum
-//   'values',     # Map
-//   'doc',
-// ])
-
-// FIELD_RESERVED_PROPS = frozenset([
-//   'default',
-//   'name',
-//   'doc',
-//   'order',
-//   'type',
-// ])
-
-// VALID_FIELD_SORT_ORDERS = frozenset([
-//   'ascending',
-//   'descending',
-//   'ignore',
-// ])
