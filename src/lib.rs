36c79c37f05449b352d83e5dbd6c2bfa41838663
Ben Evans <bevans@zendesk.com>
2016-01-26 17:31:52 -0800

    lots of boilerplate but kinda working

--- src/lib.rs
+++ src/lib.rs
@@ -11,11 +11,11 @@ mod schema {
     fields: Vec<Field>
   }
 
-  struct Field {
-    name: String,
-    doc: Option<String>,
-    default: String,
-    order: Option<Sort>
+  pub struct Field {
+    pub name: String,
+    pub doc: Option<String>,
+    pub default: String,
+    pub order: Option<Sort>
   }
 
   enum Primitive {
@@ -50,34 +50,34 @@ mod schema {
     Complex(Complex)
   }
 
-  enum Sort {
+  pub enum Sort {
     Ascending,
     Descending,
     Ignore
   }
 
   struct Message {
-  doc: Option<String>,
-  request: Vec<Field>,
-  response: Type,
-  errors: Option<Vec<Type>>,
-  one_way: Option<bool>
+    doc: Option<String>,
+    request: Vec<Field>,
+    response: Type,
+    errors: Option<Vec<Type>>,
+    one_way: Option<bool>
   }
 
-  struct Record {
-    name: String,
-    namespace: Option<String>,
-    doc: Option<String>,
-    aliases: Option<Vec<String>>,
-    fields: Vec<Field>
+  pub struct Record {
+    pub name: String,
+    pub namespace: Option<String>,
+    pub doc: Option<String>,
+    pub aliases: Option<Vec<String>>,
+    pub fields: Vec<Field>
   }
 
-  struct Enum {
-    name: String,
-    namespace: Option<String>,
-    doc: Option<String>,
-    aliases: Option<Vec<String>>,
-    symbols: Vec<String>
+  pub struct Enum {
+    pub name: String,
+    pub namespace: Option<String>,
+    pub doc: Option<String>,
+    pub aliases: Option<Vec<String>>,
+    pub symbols: Vec<String>
   }
 
   struct Array {
@@ -91,6 +91,11 @@ mod schema {
     pub size: i32
   }
 
+  struct MapVisitor<'a, T: 'a> {
+    value: &'a T,
+    state: u8,
+  }
+
   impl serde::Serialize for Fixed {
     fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
       where S: serde::Serializer
@@ -100,11 +105,6 @@ mod schema {
         state: 0,
       })
     }
-  }
-
-  struct MapVisitor<'a, T: 'a> {
-    value: &'a T,
-    state: u8,
   }
 
   impl<'a> serde::ser::MapVisitor for MapVisitor<'a, Fixed> {
@@ -134,6 +134,145 @@ mod schema {
       }
     }
   }
+
+  impl serde::Serialize for Enum {
+    fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
+      where S: serde::Serializer
+    {
+      serializer.visit_struct("enum", MapVisitor::<Enum> {
+        value: self,
+        state: 0,
+      })
+    }
+  }
+
+  impl<'a> serde::ser::MapVisitor for MapVisitor<'a, Enum> {
+      fn visit<S>(&mut self, serializer: &mut S) -> Result<Option<()>, S::Error>
+        where S: serde::Serializer
+      {
+      match self.state {
+        0 => {
+          self.state += 1;
+          Ok(Some(try!(serializer.visit_struct_elt("name", &self.value.name))))
+        }
+        1 => {
+          self.state += 1;
+          Ok(Some(try!(serializer.visit_struct_elt("namespace", &self.value.namespace))))
+        },
+        2 => {
+          self.state += 1;
+          Ok(Some(try!(serializer.visit_struct_elt("doc", &self.value.doc))))
+        },
+        3 => {
+          self.state += 1;
+          Ok(Some(try!(serializer.visit_struct_elt("aliases", &self.value.aliases))))
+        },
+        4 => {
+          self.state += 1;
+          Ok(Some(try!(serializer.visit_struct_elt("symbols", &self.value.symbols))))
+        },
+        _ => {
+          Ok(None)
+        }
+      }
+    }
+  }
+
+  impl serde::Serialize for Record {
+    fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
+      where S: serde::Serializer
+    {
+      serializer.visit_struct("record", MapVisitor::<Record> {
+        value: self,
+        state: 0,
+      })
+    }
+  }
+
+  impl<'a> serde::ser::MapVisitor for MapVisitor<'a, Record> {
+      fn visit<S>(&mut self, serializer: &mut S) -> Result<Option<()>, S::Error>
+        where S: serde::Serializer
+      {
+      match self.state {
+        0 => {
+          self.state += 1;
+          Ok(Some(try!(serializer.visit_struct_elt("name", &self.value.name))))
+        }
+        1 => {
+          self.state += 1;
+          Ok(Some(try!(serializer.visit_struct_elt("namespace", &self.value.namespace))))
+        },
+        2 => {
+          self.state += 1;
+          Ok(Some(try!(serializer.visit_struct_elt("doc", &self.value.doc))))
+        },
+        3 => {
+          self.state += 1;
+          Ok(Some(try!(serializer.visit_struct_elt("aliases", &self.value.aliases))))
+        },
+        4 => {
+          self.state += 1;
+          Ok(Some(try!(serializer.visit_struct_elt("fields", &self.value.fields))))
+        },
+        _ => {
+          Ok(None)
+        }
+      }
+    }
+  }
+
+  impl serde::Serialize for Sort {
+    fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
+      where S: serde::Serializer
+    {
+      let val = match *self {
+        Sort::Ascending => "ascending",
+        Sort::Descending => "descending",
+        Sort::Ignore => "ignore",
+      };
+      serializer.visit_map_elt("sort", val)
+    }
+  }
+
+  impl serde::Serialize for Field {
+    fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
+      where S: serde::Serializer
+    {
+      serializer.visit_struct("field", MapVisitor::<Field> {
+        value: self,
+        state: 0,
+      })
+    }
+  }
+
+  impl<'a> serde::ser::MapVisitor for MapVisitor<'a, Field> {
+      fn visit<S>(&mut self, serializer: &mut S) -> Result<Option<()>, S::Error>
+        where S: serde::Serializer
+      {
+      match self.state {
+        0 => {
+          self.state += 1;
+          Ok(Some(try!(serializer.visit_struct_elt("name", &self.value.name))))
+        }
+        1 => {
+          self.state += 1;
+          Ok(Some(try!(serializer.visit_struct_elt("doc", &self.value.doc))))
+        },
+        2 => {
+          self.state += 1;
+          Ok(Some(try!(serializer.visit_struct_elt("default", &self.value.default))))
+        },
+        3 => {
+          self.state += 1;
+          Ok(Some(try!(serializer.visit_struct_elt("order", &self.value.order))))
+        },
+        _ => {
+          Ok(None)
+        }
+      }
+    }
+  }
+
 }
 
 #[test]
